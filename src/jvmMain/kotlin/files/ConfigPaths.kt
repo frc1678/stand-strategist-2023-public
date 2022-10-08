61e16fa2c7f9d3ba37f3c83ee628a3b21334c79f
Edwin Chang <edwinchang2024@gmail.com>
2022-10-08 13:59:37 -0700

    Add comments

--- src/jvmMain/kotlin/files/ConfigPaths.kt
+++ src/jvmMain/kotlin/files/ConfigPaths.kt
@@ -2,6 +2,10 @@ package files
 
 import java.io.File
 
+/**
+ * The main config folder used by the app.
+ * Should be something like `C:/Users/username/AppData/Stand Strategist/` or `/home/username/.stand-strategist`.
+ */
 val MAIN_FOLDER = if (System.getProperty("os.name").contains("win", true)) {
     // windows
     File(File(System.getenv("APPDATA"), "Local"), "Stand Strategist")
@@ -10,10 +14,22 @@ val MAIN_FOLDER = if (System.getProperty("os.name").contains("win", true)) {
     File(System.getProperty("user.home"), ".stand-strategist")
 }
 
+/**
+ * The full path to the match schedule file.
+ */
 val MATCH_SCHEDULE_FILE = File(MAIN_FOLDER, "match_schedule.json")
 
+/**
+ * The full path to the Team-In-Match data file.
+ */
 val TIM_DATA_FILE = File(MAIN_FOLDER, "tim_data.csv")
 
+/**
+ * The full path to the team data file.
+ */
 val TEAM_DATA_FILE = File(MAIN_FOLDER, "team_data.csv")
 
+/**
+ * The full path to the settings file.
+ */
 val SETTINGS_FILE = File(MAIN_FOLDER, "settings.json")
--- src/jvmMain/kotlin/files/DebouncedFileWriter.kt
+++ src/jvmMain/kotlin/files/DebouncedFileWriter.kt
@@ -1,5 +1,6 @@
 package files
 
+import androidx.compose.runtime.LaunchedEffect
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
@@ -7,10 +8,32 @@ import kotlinx.coroutines.withContext
 import java.io.File
 import kotlin.time.Duration.Companion.milliseconds
 
+/**
+ * The debouncing duration for file writes.
+ */
 val DEBOUNCE = 1000.milliseconds
 
+/**
+ * A class for managing file writing operations with debouncing.
+ *
+ * Debouncing ensures that there is a delay between file writes,
+ * so that not too many will happen in a short period of time.
+ *
+ * Instances of this class should have:
+ * - the [start] method called from the [LaunchedEffect] of [Observer], and
+ * - the [writeData] method called whenever the corresponding data is updated (use [Observer]).
+ *
+ * @param file The [File] to write to.
+ * @param deserializer A lambda function that takes the serialized data as a parameter
+ * and returns a `String` version ready to be written to the [file].
+ * @see Observer
+ */
 class DebouncedFileWriter<T>(private val file: File, private val deserializer: (T) -> String) {
 
+    /**
+     * Starts listening for new data and writes the new data to the [file].
+     * Applies debouncing when the data are updated quickly.
+     */
     suspend fun start() {
         for (data in channel) withContext(Dispatchers.IO) {
             if (!file.exists()) file.createNewFile()
@@ -19,8 +42,14 @@ class DebouncedFileWriter<T>(private val file: File, private val deserializer: (
         }
     }
 
+    /**
+     * The [Channel] that all the data are sent through to the debouncing coroutine.
+     */
     private val channel = Channel<T>(Channel.CONFLATED)
 
+    /**
+     * Writes the given [data] to the file. This won't write immediately if there is debouncing happening.
+     */
     suspend fun writeData(data: T) {
         channel.send(data)
     }
