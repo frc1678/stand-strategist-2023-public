228d313a98b5a2fe6c209b6262f12a4ab7c0a8e8
Edwin Chang <edwinchang2024@gmail.com>
2022-10-07 21:00:03 -0700

    Add file writing thing

---
+++ src/jvmMain/kotlin/files/FileWriter.kt
@@ -0,0 +1,26 @@
+package files
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import java.io.File
+import kotlin.time.Duration.Companion.milliseconds
+
+val DEBOUNCE = 1000.milliseconds
+
+class FileWriter<T>(private val file: File, private val deserializer: (T) -> String) {
+
+    suspend fun start() {
+        while (true) {
+            val data = channel.receive()
+            file.writeText(deserializer(data))
+            delay(DEBOUNCE)
+        }
+    }
+
+    private val channel = Channel<T>(Channel.CONFLATED)
+
+    fun writeData(data: T) {
+        channel.trySend(data)
+    }
+
+}
