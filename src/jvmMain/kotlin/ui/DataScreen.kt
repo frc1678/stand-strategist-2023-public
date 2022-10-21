638595fcddc4417a7c47a8ae9321ba258956058c
cyan-ding <blueplus.d@gmail.com>
2022-10-20 20:50:31 -0700

    did no

--- src/jvmMain/kotlin/ui/DataScreen.kt
+++ src/jvmMain/kotlin/ui/DataScreen.kt
@@ -1,7 +1,5 @@
 package ui
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
@@ -16,10 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -30,10 +26,19 @@ fun Columns(x: Int) {
     var text by remember { mutableStateOf("") }
     var text2 by remember { mutableStateOf("") }
     var text3 by remember { mutableStateOf("") }
+    val checkedState = remember { mutableStateOf(true) }
+    val checkedState2 = remember { mutableStateOf(true) }
+    val checkedState3 = remember { mutableStateOf(true) }
+    val checkedState4 = remember { mutableStateOf(true) }
+    val checkedState5 = remember { mutableStateOf(true) }
+    val checkedState6 = remember { mutableStateOf(true) }
     when (x) {
         0 -> {
 
-            Column(modifier = Modifier.padding(horizontal = 10.dp).fillMaxHeight().padding(top= 90.dp), verticalArrangement = Arrangement.spacedBy(170.dp)) {
+            Column(
+                modifier = Modifier.padding(horizontal = 10.dp).fillMaxHeight().padding(top = 90.dp),
+                verticalArrangement = Arrangement.spacedBy(170.dp)
+            ) {
                 Text("")
                 Text("Team 1")
                 Text("Team 2")
@@ -42,8 +47,86 @@ fun Columns(x: Int) {
 
             }
         }
+
+        1 -> {
+            Column(
+                modifier = Modifier.padding(horizontal = 10.dp).padding(top = 90.dp).fillMaxHeight(),
+                verticalArrangement = Arrangement.spacedBy(125.dp)
+            ) {
+                Text(dataPoints[x])
+
+                Checkbox(
+
+                    checked = checkedState.value,
+
+                    modifier = Modifier.padding(16.dp),
+
+                    onCheckedChange = { checkedState.value = it },
+                )
+
+                Checkbox(
+
+                    checked = checkedState2.value,
+
+                    modifier = Modifier.padding(16.dp),
+
+                    onCheckedChange = { checkedState2.value = it },
+                )
+
+                Checkbox(
+
+                    checked = checkedState3.value,
+
+                    modifier = Modifier.padding(16.dp),
+
+                    onCheckedChange = { checkedState3.value = it },
+                )
+
+            }
+        }
+
+        3 -> {
+            Column(
+                modifier = Modifier.padding(horizontal = 10.dp).padding(top = 90.dp).fillMaxHeight(),
+                verticalArrangement = Arrangement.spacedBy(125.dp)
+            ) {
+                Text(dataPoints[x])
+
+                Checkbox(
+
+                    checked = checkedState4.value,
+
+                    modifier = Modifier.padding(16.dp),
+
+                    onCheckedChange = { checkedState4.value = it },
+                )
+
+                Checkbox(
+
+                    checked = checkedState5.value,
+
+                    modifier = Modifier.padding(16.dp),
+
+                    onCheckedChange = { checkedState5.value = it },
+                )
+
+                Checkbox(
+
+                    checked = checkedState6.value,
+
+                    modifier = Modifier.padding(16.dp),
+
+                    onCheckedChange = { checkedState6.value = it },
+                )
+
+            }
+        }
+
         5 -> {
-            Column(modifier = Modifier.padding(horizontal = 10.dp).padding(top= 90.dp).fillMaxHeight(), verticalArrangement = Arrangement.spacedBy(145.dp)) {
+            Column(
+                modifier = Modifier.padding(horizontal = 10.dp).padding(top = 90.dp).fillMaxHeight(),
+                verticalArrangement = Arrangement.spacedBy(145.dp)
+            ) {
                 Text(dataPoints[x])
                 TextField(
                     value = text,
@@ -63,7 +146,10 @@ fun Columns(x: Int) {
             }
         }
         else -> {
-            Column(modifier = Modifier.padding(horizontal = 10.dp).padding(top= 90.dp).fillMaxHeight(), verticalArrangement = Arrangement.spacedBy(145.dp)) {
+            Column(
+                modifier = Modifier.padding(horizontal = 10.dp).padding(top = 90.dp).fillMaxHeight(),
+                verticalArrangement = Arrangement.spacedBy(145.dp)
+            ) {
                 Text(dataPoints[x])
                 TextField(
                     value = text,
@@ -106,8 +192,8 @@ fun DataScreen() {
         Columns(5)
 
 
-
-}}
+    }
+}
 
 
 
